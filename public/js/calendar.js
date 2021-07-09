e80071af9ab01c86d7a5ec6084e9470129669b17
TaKuYa YaNaSe <yt.baseball1168@gmail.com>
2021-07-09 15:07:50 +0900

    First Commit

---
+++ public/js/calendar.js
@@ -0,0 +1,157 @@
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+// 月末だとずれる可能性があるため、1日固定で取得
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// 初期表示
+window.onload = function () {
+  showProcess(today, calendar);
+};
+// 前の月表示
+function prev() {
+  showDate.setMonth(showDate.getMonth() - 1);
+  showProcess(showDate);
+}
+
+// 次の月表示
+function next() {
+  showDate.setMonth(showDate.getMonth() + 1);
+  showProcess(showDate);
+}
+
+// カレンダー表示
+function showProcess(date) {
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  document.querySelector("#header").innerHTML =
+    year + "年 " + (month + 1) + "月";
+
+  var calendar = createProcess(year, month);
+  document.querySelector("#calendar").innerHTML = calendar;
+}
+
+// カレンダー作成
+function createProcess(year, month) {
+  // 曜日
+  var calendar = "<table><tr class='dayOfWeek'>";
+  for (var i = 0; i < week.length; i++) {
+    calendar += "<th>" + week[i] + "</th>";
+  }
+  calendar += "</tr>";
+
+  var count = 0;
+  var startDayOfWeek = new Date(year, month, 1).getDay();
+  var endDate = new Date(year, month + 1, 0).getDate();
+  var lastMonthEndDate = new Date(year, month, 0).getDate();
+  var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+  // 1行ずつ設定
+  for (var i = 0; i < row; i++) {
+    calendar += "<tr>";
+    // 1colum単位で設定
+    for (var j = 0; j < week.length; j++) {
+      if (i == 0 && j < startDayOfWeek) {
+        // 1行目で1日まで先月の日付を設定
+        calendar +=
+          "<td class='disabled'>" +
+          (lastMonthEndDate - startDayOfWeek + j + 1) +
+          "</td>";
+      } else if (count >= endDate) {
+        // 最終行で最終日以降、翌月の日付を設定
+        count++;
+        calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+      } else {
+        // 当月の日付を曜日に照らし合わせて設定
+        count++;
+        //日にちを文字列として保存
+
+        var date = year.toString() + (month + 1).toString() + count.toString();
+        // 選択されたカレンダーの日時を取得
+        if (
+          year == today.getFullYear() &&
+          month == today.getMonth() &&
+          count == today.getDate()
+        ) {
+          calendar +=
+            "<td id=" +
+            date +
+            " class='today' onclick=selectDate(" +
+            date +
+            "," +
+            year +
+            "," +
+            month +
+            "," +
+            count +
+            ")>" +
+            count +
+            "</td>";
+        } else {
+          calendar +=
+            "<td id=" +
+            date +
+            " onclick=selectDate(" +
+            date +
+            "," +
+            year +
+            "," +
+            month +
+            "," +
+            count +
+            ")>" +
+            count +
+            "</td>";
+        }
+      }
+    }
+    calendar += "</tr>";
+  }
+  return calendar;
+}
+
+//選択されたセルを一時保存する配列
+var seletedCell = null;
+
+// カレンダーを選択したときのイベント
+function selectDate(date, year, month, day) {
+  changeCellColor(date);
+  setDate(year, month, day);
+}
+
+//選択されたセルの色を変更
+function changeCellColor(date) {
+  //選択されたセルを取得
+  cell = document.getElementById(date);
+
+  //セルが選択されているかを判別
+  if (seletedCell == null) {
+    //セルが1つも選択されていない場合
+    seletedCell = cell;
+    seletedCell.style.background = "rgb(190, 190, 190)";
+  } else if (seletedCell == cell) {
+    //同じセルを選択したとき
+    seletedCell.style.background = "white";
+    seletedCell = null;
+  } else {
+    //違うセルを選択したとき
+    seletedCell.style.background = "white";
+    seletedCell = cell;
+    seletedCell.style.background = "rgb(190, 190, 190)";
+  }
+}
+
+//選択された日付を表示する
+function setDate(year, month, day) {
+  console.log(year, month + 1, day);
+  //日付を変換
+  convertedDate =
+    year.toString() +
+    "年" +
+    (month + 1).toString() +
+    "月" +
+    day.toString() +
+    "日";
+  //選択された日付を表示する要素
+  selectedDate = document.getElementById("selectedDate");
+  selectedDate.innerHTML = convertedDate;
+}
